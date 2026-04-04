3c25366d5c22809561adae5260f7498ae995fdc9
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-04-04 13:02:09 +0800

    feat(crates/rotating-file): add async log file rotation
    - Implement a new `ocelot-rotating-file` crate for asynchronous log rotation.
    - Provide configurable size and time thresholds to trigger file rotation.
    - Support compression of rotated files using `gzip` and `LZ4` algorithms.
    - Manage rotated files by enforcing limits on maximum file count and age.
    - Integrate the new `ocelot-rotating-file` crate into the workspace.

---
+++ crates/rotating-file/src/compress.rs
@@ -0,0 +1,21 @@
+use std::{
+    io,
+    io::{Read, Write},
+};
+
+use flate2::write::GzEncoder;
+use lz4_flex::frame::FrameEncoder as Lz4FrameEncoder;
+
+pub fn compress_gzip<R: Read, W: Write>(source: &mut R, destination: &mut W) -> io::Result<()> {
+    let mut encoder = GzEncoder::new(destination, flate2::Compression::default());
+    let _ = io::copy(source, &mut encoder)?;
+    let _ = encoder.finish()?;
+    Ok(())
+}
+
+pub fn compress_lz4<R: Read, W: Write>(source: &mut R, destination: &mut W) -> io::Result<()> {
+    let mut encoder = Lz4FrameEncoder::new(destination);
+    let _ = io::copy(source, &mut encoder)?;
+    let _ = encoder.finish()?;
+    Ok(())
+}
